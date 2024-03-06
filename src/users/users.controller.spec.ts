47e25ce2264d5c0b2a50a9d94e5552512e64b3f3
Alexandru Tudor <alexandru-adrian.tudor@endava.com>
2024-03-06 16:13:01 +0200

    remove tests from pipeline

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from '../application/users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let userServiceMock: Partial<UsersService>;
 
   beforeEach(async () => {
+    userServiceMock = {
+      // Mock any methods you need for testing
+      // For example:
+      findAll: jasmine.createSpy('findAll').and.returnValue([]), // Mocking findAll method
+      findOne: jasmine.createSpy('findOne').and.returnValue({}), // Mocking findOne method
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: userServiceMock }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
